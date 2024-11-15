c8c1785673c6cf8c826c04fd1aa248a8b3799c04
ayushrajdahal <ayushrajdahal@gmail.com>
2024-11-15 03:37:36 -0600

    Add publish and action YMLs, switch to build config in NextJS.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
+    basePath: "/uno-acm-website",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
