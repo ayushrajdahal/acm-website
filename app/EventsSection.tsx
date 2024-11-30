36e79e2a59ee4547ad92b3556b4edb89617c7d10
ayushrajdahal <ayushrajdahal@gmail.com>
2024-11-29 23:16:57 -0600

    Remove unused useEffect import in EventsSection component.

--- app/EventsSection.tsx
+++ app/EventsSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { events } from "./lists.json";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
