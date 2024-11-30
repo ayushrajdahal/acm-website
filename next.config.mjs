fbaa9c538afdddd00a5f2453b00f551bdecbf64c
ayushrajdahal <ayushrajdahal@gmail.com>
2024-11-29 23:14:38 -0600

    Remove unused configuration options from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/",
-    images: {
-        unoptimized: true,
-      },
-    output: "export",  // <=== enables static exports
 };
 
 export default nextConfig;
