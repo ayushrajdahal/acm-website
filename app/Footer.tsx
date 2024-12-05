5ac51f3f7c965b60cdf41c5b24a49121f6cd9301
ayushrajdahal <ayushrajdahal@gmail.com>
2024-12-05 14:28:44 -0600

    Auto-update copyright year in footer.

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="mt-32 text-center text-gray-500 border-t border-gray-100 pt-8">
-      <p>&copy; 2024 UNO ACM. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} UNO ACM. All rights reserved.</p>
     </footer>
   );
 };
