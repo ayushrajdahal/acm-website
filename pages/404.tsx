95312a8d74482e608c0cf374e9f423928980ea7e
ayushrajdahal <ayushrajdahal@gmail.com>
2024-12-06 11:01:23 -0600

    Add onClick trigger in 404 page.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 
 const Custom404 = () => {
+  const [spinState, setSpinState] = useState(false);
+
+  const swapSpinState = () => {
+    setSpinState(!spinState);
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="text-center">
@@ -11,14 +18,13 @@ const Custom404 = () => {
           alt="ACM Logo"
           width={80}
           height={80}
-          className="mb-8"
+          className={`mb-8 ${spinState === true ? "animate-spin" : ""}`}
+          onClick={swapSpinState}
         />
         <h1 className="text-6xl font-bold mb-4">404</h1>
         <p className="text-2xl mb-8">Page Not Found</p>
-        <Link href="/">
-          <a className="text-lg text-blue-500 hover:underline">
-            Go back to Home
-          </a>
+        <Link href="/" className="text-lg text-blue-500 hover:underline">
+          Go back to Home
         </Link>
       </div>
     </div>
